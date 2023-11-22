6f7f8d1646dd1eecea15fd1c035fb8afe438a3fa
Diyar GUNGOR <Diyar.GUNGOR@student.hel.be>
2023-11-22 01:40:11 -0500

    Add image

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -15,85 +15,85 @@ namespace TestProject1
         [Test]
         public void TestRendueDeMonnaie_8()
         {
-            int montant = 8;
+            int Amount = 8;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
 
-            Assert.AreEqual("4 * 2", result);
+            Assert.AreEqual("4 * 2", Result);
         }
         [Test]
         public void TestRendueDeMonnaie_9()
         {
-            int montant = 9;
+            int Amount = 9;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
 
-            Assert.AreEqual("2 * 2 + 1 * 5", result);
+            Assert.AreEqual("2 * 2 + 1 * 5", Result);
         }
 
         [Test]
         public void TestRendueDeMonnaie_10()
         {
-            int montant = 10;
+            int Amount = 10;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
 
-            Assert.AreEqual("1 * 10", result);
+            Assert.AreEqual("1 * 10", Result);
         }
         [Test]
         public void TestRendueDeMonnaie_11()
         {
-            int montant = 11;
+            int Amount = 11;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
 
-            Assert.AreEqual("3 * 2 + 1 * 5", result);
+            Assert.AreEqual("3 * 2 + 1 * 5", Result);
         }
         [Test]
         public void TestRendueDeMonnaie_21()
         {
-            int montant = 21;
+            int Amount = 21;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
 
-            Assert.AreEqual("3 * 2 + 1 * 5 + 1 * 10", result);
+            Assert.AreEqual("3 * 2 + 1 * 5 + 1 * 10", Result);
         }
         [Test]
         public void TestRendueDeMonnaie_20()
         {
-            int montant = 20;
+            int Amount = 20;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
 
-            Assert.AreEqual("2 * 10", result);
+            Assert.AreEqual("2 * 10", Result);
         }
         [Test]
         public void TestRendueDeMonnaie_23()
         {
-            int montant = 23;
+            int Amount = 23;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
 
-            Assert.AreEqual("4 * 2 + 1 * 5 + 1 * 10", result);
+            Assert.AreEqual("4 * 2 + 1 * 5 + 1 * 10", Result);
         }
         [Test]
         public void TestRendueDeMonnaie_31()
         {
-            int montant = 31;
+            int Amount = 31;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
 
-            Assert.AreEqual("3 * 2 + 1 * 5 + 2 * 10", result);
+            Assert.AreEqual("3 * 2 + 1 * 5 + 2 * 10", Result);
         }
 
         [Test]
         public void TestRendueDeMonnaie_9007199254740991()
         {
-            long montant = 9007199254740991;
+            long Amount = 9007199254740991;
 
-            var result = _rdm.BankNotes(montant);
+            var Result = _rdm.BankNotes(Amount);
             //10 * 900 719 925 474 098 + 5 + 2 * 3
-            Assert.AreEqual("3 * 2 + 1 * 5 + 900719925474098 * 10", result);
+            Assert.AreEqual("3 * 2 + 1 * 5 + 900719925474098 * 10", Result);
         }
     }
 }
